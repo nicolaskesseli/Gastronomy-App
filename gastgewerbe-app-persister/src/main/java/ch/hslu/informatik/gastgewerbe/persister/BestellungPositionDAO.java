3df9736773ed4a7889b87538b7e93e86d7dfd577
iatrippe <andreas.trippel@stud.hslu.ch>
2018-04-26 00:33:51 +0200

    Testklasse Bestellung finito

---
+++ gastgewerbe-app-persister/src/main/java/ch/hslu/informatik/gastgewerbe/persister/BestellungPositionDAO.java
@@ -0,0 +1,4 @@
+package ch.hslu.informatik.gastgewerbe.persister;
+
+public interface BestellungPositionDAO {
+}
---
+++ gastgewerbe-app-persister/src/main/java/ch/hslu/informatik/gastgewerbe/persister/impl/BestellungPositionDAOImpl.java
@@ -0,0 +1,4 @@
+package ch.hslu.informatik.gastgewerbe.persister.impl;
+
+public class BestellungPositionDAOImpl {
+}
