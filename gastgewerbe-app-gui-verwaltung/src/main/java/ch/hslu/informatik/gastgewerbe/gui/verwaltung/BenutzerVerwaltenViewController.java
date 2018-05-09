6b8182f8e0e81b20ff9ebe0911e1bbc6fff77664
iaacherm <simon.achermann@stud.hslu.ch>
2018-05-09 13:29:27 +0200

    Verwaltungs-GUI

---
+++ gastgewerbe-app-gui-verwaltung/src/main/java/ch/hslu/informatik/gastgewerbe/gui/verwaltung/BenutzerVerwaltenViewController.java
@@ -0,0 +1,134 @@
+package ch.hslu.informatik.gastgewerbe.gui.verwaltung;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class BenutzerVerwaltenViewController {
+
+	private static Logger logger = LogManager.getLogger(BenutzerVerwaltenViewController.class);
+	
+    @FXML
+    private ComboBox<?> cmbRolle;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField vornameInput;
+
+    @FXML
+    private TextField strasseInput;
+
+    @FXML
+    private TextField plzInput;
+
+    @FXML
+    private TextField ortInput;
+
+    @FXML
+    private TextField emailInput;
+
+    @FXML
+    private TextField telefonInput;
+
+    @FXML
+    private TextField benutzernameInput;
+
+    @FXML
+    private TextField kennwortInput;
+
+    @FXML
+    private Button spiechernBtn;
+
+    @FXML
+    private Button neuBtn;
+
+    @FXML
+    private Button resetBtn;
+
+    @FXML
+    private TableView<?> tblBenutzer;
+
+    @FXML
+    private TableColumn<?, ?> colNummer;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colVorname;
+
+    @FXML
+    private TableColumn<?, ?> colStrasse;
+
+    @FXML
+    private TableColumn<?, ?> colPlz;
+
+    @FXML
+    private TableColumn<?, ?> colOrt;
+
+    @FXML
+    private TableColumn<?, ?> colRolle;
+
+    @FXML
+    private Button löschenBtn;
+
+    @FXML
+    private Button zurückBtn;
+
+    @FXML
+    void loeschen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void neuenBenutzerErfassen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void speichern(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zurück(ActionEvent event) {
+    	try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainWindowVerwaltung.fxml"));
+			Parent root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setTitle("Hauptseite");
+			stage.setScene(new Scene(root1));
+			stage.show();
+			((Node) (event.getSource())).getScene().getWindow().hide();
+    	} catch (IOException e) {
+			logger.error(e.getMessage(), e);
+
+		}
+    }
+
+}
---
+++ gastgewerbe-app-gui-verwaltung/src/main/java/ch/hslu/informatik/gastgewerbe/gui/verwaltung/ProduktVerwaltenViewController.java
@@ -0,0 +1,108 @@
+package ch.hslu.informatik.gastgewerbe.gui.verwaltung;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ProduktVerwaltenViewController {
+
+	private static Logger logger = LogManager.getLogger(ProduktVerwaltenViewController.class);
+
+	@FXML
+	private TextField bezeichnungInput;
+
+	@FXML
+	private TextField nummerInput;
+
+	@FXML
+	private TextField preisInput;
+
+	@FXML
+	private ComboBox<?> kategorieCmb;
+
+	@FXML
+	private TextField txtBeschreibung;
+
+	@FXML
+	private Button hinzufuegenBtn;
+
+	@FXML
+	private Button resetBtn;
+
+	@FXML
+	private TableView<?> produktTbl;
+
+	@FXML
+	private TableColumn<?, ?> colNummer;
+
+	@FXML
+	private TableColumn<?, ?> colLieferant;
+
+	@FXML
+	private TableColumn<?, ?> colProduktTypCode;
+
+	@FXML
+	private TableColumn<?, ?> colPreis;
+
+	@FXML
+	private Button loeschenBtn;
+
+	@FXML
+	private TextField nummerLöschenInput;
+
+	@FXML
+	private Button suchenBtn;
+
+	@FXML
+	void hinzufügen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void loeschen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void reset(ActionEvent event) {
+
+	}
+
+	@FXML
+	void suchen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void zurück(ActionEvent event) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainWindowVerwaltung.fxml"));
+			Parent root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setTitle("Hauptseite");
+			stage.setScene(new Scene(root1));
+			stage.show();
+			((Node) (event.getSource())).getScene().getWindow().hide();
+		} catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+}
