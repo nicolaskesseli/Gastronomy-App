38dc8f9702723ac5f0bdd65f63cbb9a7eb4c97e4
iaschwen <thomas.schwendimann@stud.hslu.ch>
2018-05-09 11:37:29 +0200

    GUI Bar angepasst

---
+++ gastgewerbe-app-gui-bar/src/main/java/ch/hslu/informatik/gastgewerbe/gui/bar/MainWindowBarController.java
@@ -0,0 +1,67 @@
+package ch.hslu.informatik.gastgewerbe.gui.bar;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class MainWindowBarController {
+
+	private static Logger logger = LogManager.getLogger(MainWindowBarController.class);
+	
+	    @FXML
+	    private TableView<?> bestellübersichtBarTbl;
+
+	    @FXML
+	    private Button bestellungBereitBtn;
+
+	    @FXML
+	    private Button bestellungLöschenBtn;
+	    
+	    @FXML 
+	    private Button abmeldeBtn;
+	    
+	    @FXML
+	    void abmelden(ActionEvent event) {
+	    	try {
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/LoginView.fxml"));
+				Parent root1 = (Parent) fxmlLoader.load();
+				Stage stage = new Stage();
+				stage.initModality(Modality.APPLICATION_MODAL);
+				stage.initStyle(StageStyle.UNDECORATED);
+				stage.setScene(new Scene(root1));
+				stage.show();
+				((Node) (event.getSource())).getScene().getWindow().hide();
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+
+			}
+	    }
+
+	    /*  @FXML
+	    void bestellungBereit(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void bestellungLöschen(ActionEvent event) {
+
+	    } */
+
+	}
+
+	
+
