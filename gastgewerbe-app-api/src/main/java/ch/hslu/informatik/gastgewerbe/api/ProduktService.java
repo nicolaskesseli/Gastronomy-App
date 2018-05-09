e4526b8832890982d23c837b7b65aff117cdbcdd
iakessel <nicolas.kesseli@stud.hslu.ch>
2018-05-09 12:59:41 +0200

    Implementierung Produkt

--- gastgewerbe-app-api/src/main/java/ch/hslu/informatik/gastgewerbe/api/ProduktService.java
+++ gastgewerbe-app-api/src/main/java/ch/hslu/informatik/gastgewerbe/api/ProduktService.java
@@ -2,35 +2,30 @@ package ch.hslu.informatik.gastgewerbe.api;
 
 import java.util.List;
 
+import ch.hslu.informatik.gastgewerbe.model.KategorieTyp;
 import ch.hslu.informatik.gastgewerbe.model.Produkt;
 
 public interface ProduktService {
 
 	//Fügt neuen Artikel hinzu
 	Produkt produktHinzufuegen(Produkt produkt) throws Exception;
-	
-	
+
 	//Löscht Artikel
 	void produktLoeschen(Produkt produkt) throws Exception;
-	
-	
+
 	//Ändert bestehenden Artikel
 	Produkt produktAktualisieren(Produkt produkt) throws Exception;
 	
 	//Findet Artikel mit ent. produktCode
-	Produkt findByProduktCode(String produktCode) throws Exception;
+	List<Produkt> findByProduktCode(String produktCode) throws Exception;
 
 	//Findet Liste von Produkte anhand des Namen
 	List<Produkt> findProduktByName(String name) throws Exception;
-	
-	
+
+	//Zeigt alle Produkte aus gewünschter Kategorie (Getränk / Gericht / SNACK)
+	List<Produkt> findProduktByKategorie(KategorieTyp kategorie) throws Exception;
+
 	//Zeigt alle Pordukte an
 	List<Produkt> alleProdukt()throws Exception;
-	
-	
-	//Zeigt alle Produkte aus gewünschter Kategorie (Getränk / Gericht / SNACK)
-	List<Produkt> alleProduktNachKategorie(String kategorie) throws Exception;
-	
-	
-	
+
 }
--- gastgewerbe-app-business-produkt/src/main/java/ch/hslu/informatik/gastgewerbe/businessprodukt/ProduktManager.java
+++ gastgewerbe-app-business-produkt/src/main/java/ch/hslu/informatik/gastgewerbe/businessprodukt/ProduktManager.java
@@ -1,47 +1,109 @@
 package ch.hslu.informatik.gastgewerbe.businessprodukt;
 
 import ch.hslu.informatik.gastgewerbe.api.ProduktService;
+import ch.hslu.informatik.gastgewerbe.model.KategorieTyp;
 import ch.hslu.informatik.gastgewerbe.model.Produkt;
+import ch.hslu.informatik.gastgewerbe.persister.PersonDAO;
+import ch.hslu.informatik.gastgewerbe.persister.ProduktDAO;
+import ch.hslu.informatik.gastgewerbe.persister.impl.ProduktDAOImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProduktManager implements ProduktService {
 
+    private static Logger logger = LogManager.getLogger(ProduktManager.class);
+
+    private ProduktDAO produktDAO;
+
+    public ProduktDAO getProduktDAO() {
+
+        if (produktDAO == null) {
+            produktDAO = new ProduktDAOImpl();
+        }
+
+        return produktDAO;
+    }
+
     @Override
     public Produkt produktHinzufuegen(Produkt produkt) throws Exception {
-        return null;
+        try {
+            return getProduktDAO().save(produkt);
+        } catch (Exception e) {
+            String msg = "Produkt \'" + produkt.getName() + " " + produkt.getBeschreibung()
+                    + "\' konnte nicht hinzugefügt werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
     }
 
     @Override
     public void produktLoeschen(Produkt produkt) throws Exception {
-
+        try {
+            getProduktDAO().delete(produkt);
+        } catch (Exception e) {
+            String msg = "Produkt \'" + produkt.getName() + " " + produkt.getBeschreibung()
+                    + "\' konnte nicht gelöscht werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
     }
 
     @Override
     public Produkt produktAktualisieren(Produkt produkt) throws Exception {
-        return null;
+        try {
+            return getProduktDAO().update(produkt);
+        } catch (Exception e) {
+            String msg = "Produkt \'" + produkt.getName() + " " + produkt.getBeschreibung()
+                    + "\' konnte nicht gelöscht werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
     }
 
-
     @Override
-    public List<Produkt> alleProdukt() {
-        return null;
+    public List<Produkt> alleProdukt() throws Exception{
+        try {
+            return getProduktDAO().findAll();
+        } catch (Exception e) {
+            String msg = "Produkte konnten nicht gefunden werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
     }
 
     @Override
-    public List<Produkt> alleProduktNachKategorie(String kategorie) {
-        return null;
+    public List<Produkt> findProduktByKategorie(KategorieTyp kategorieTyp) throws Exception{
+        try {
+            return getProduktDAO().findByKategorie(kategorieTyp);
+        } catch (Exception e) {
+            String msg = "Produkt als \'" + kategorieTyp.bezeichnung() + "\' konnten nicht gefunden werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
     }
 
 	@Override
-	public Produkt findByProduktCode(String produktCode) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Produkt> findByProduktCode(String produktCode) throws Exception {
+        try {
+            return getProduktDAO().findByProduktCode(produktCode);
+        } catch (Exception e) {
+            String msg = "Produkt als \'" + produktCode + "\' konnten nicht gefunden werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
 	}
 
 	@Override
 	public List<Produkt> findProduktByName(String name) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+        try {
+            return getProduktDAO().findByName(name);
+        } catch (Exception e) {
+            String msg = "Produkt \'" + name + "\' konnte nicht gefunden werden";
+            logger.error(msg, e);
+            throw new Exception(msg);
+        }
 	}
 }
