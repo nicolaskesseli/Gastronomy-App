41c32d26d3354fa7b182b5964c4fbd527fa31ff5
iaacherm <simon.achermann@stud.hslu.ch>
2018-05-10 13:48:15 +0200

    Funktion, um in GUI-Tabelle zur schreiben ergänzt.

--- gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/BestellungErfassenController.java
+++ gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/BestellungErfassenController.java
@@ -1,33 +1,39 @@
 	package ch.hslu.informatik.gastgewerbe.gui.kellner;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import ch.hslu.informatik.gastgewerbe.model.Bestellung;
 import ch.hslu.informatik.gastgewerbe.model.Produkt;
 import ch.hslu.informatik.gastgewerbe.persister.impl.ProduktDAOImpl;
 
-	public class BestellungErfassenController {
+	public class BestellungErfassenController implements Initializable {
 
 		private static Logger logger = LogManager.getLogger(BestellungErfassenController.class);
-		
-	
 		
 	    @FXML
 	    private TextField tischNrInput;
@@ -36,7 +42,13 @@ import ch.hslu.informatik.gastgewerbe.persister.impl.ProduktDAOImpl;
 	    private TableView<?> bestellübersichtTbl;
 
 	    @FXML
-	    private TextField gerichtNrInput;
+	    private TableColumn<Table, String> code;
+
+	    @FXML
+	    private TableColumn<Table, String> bezeichnung2;
+
+	    @FXML
+	    private TableColumn<Table, String> bemerkung;
 
 	    @FXML
 	    private Button bestellungAbschickenBtn;
@@ -48,48 +60,95 @@ import ch.hslu.informatik.gastgewerbe.persister.impl.ProduktDAOImpl;
 	    private Button zurückInput;
 
 	    @FXML
-	    private TableView<?> gerichtAuswahl;
-
-	    @FXML
 	    private Button gerichtHinzufügenBtn;
 
 	    @FXML
+	    private TextField gerichtNrInput;
+
+	    @FXML
 	    private Button suchenGerichtBtn;
-	    
-	    @FXML 
-	    private Button gerichtNameInput;
+
+	    @FXML
+	    private TextField gerichtNameInput;
+
+	    @FXML
+	    private Button suchenGerichtBtn1;
+
+	    @FXML
+	    private TableView<Table> gerichtAuswahl;
+
+	    @FXML
+	    private TableColumn<Table, Double> preis;
 	    
 	    @FXML
-	    private Label labelGericht;
-
+	    private TableColumn<Table, String> bezeichnung;
+	    
+	    
+	    
+	    final ObservableList<Table> data = FXCollections.observableArrayList(
+//				new Table(p.getBeschreibung(), p.getPreis()),
+				new Table("12.50", 100)
+				);
+				
+				
+	    
+	    
+	    
 	    @FXML
 	    void bestellungAbschicken(ActionEvent event) {
-
+	    	Bestellung bestellung = new Bestellung();
+	    	
+	    	try{
+	    		
+	    	Context.getInstance().getBestellungService().bestellen(bestellung);	
+	    	
+	    	}catch(Exception e) {
+	    		e.getMessage();
+	    		System.out.println("Ein Fehler ist aufgetreten");
+	    	}
+	    	
+	    	
 	    }
 
 	    @FXML
 	    void bestellungHinzufügen(ActionEvent event) {
-
+	    	
 	    }
 
 	    @FXML
 	    void gerichtSuchen(ActionEvent event) {
+	    	
 	    	String gerichtCode = gerichtNrInput.getText();
 	    	Produkt p;
 	    	
 	    	try{
+	    		
 	    		p = Context.getInstance().getProduktService().findByProduktCode(gerichtCode);
 	    		
-	    		
-	    
+	    	
 	    	}catch(Exception e) {
 	    		e.getMessage();
+	    		System.out.println("Ein Fehler ist aufgetreten");
 	    	}
 	    }
 	    
+	    @FXML
 	    void gerichtSuchenName(ActionEvent event) {
+
+	    	String gerichtName = gerichtNrInput.getText();
+	    	List<Produkt> pListe = new ArrayList<>();
 	    	
+	    	try{
+	    		
+	    		pListe = Context.getInstance().getProduktService().findProduktByName(gerichtName);
+	     		
+	    
+	    	}catch(Exception e) {
+	    		e.getMessage();
+	    		System.out.println("Ein Fehler ist aufgetreten");
+	    	}
 	    }
+
 
 	    @FXML
 	    void zuruck(ActionEvent event) {
@@ -108,6 +167,14 @@ import ch.hslu.informatik.gastgewerbe.persister.impl.ProduktDAOImpl;
 
 			}
 	    }
+	    
+	    public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("Initialize aufgerufen");
+	    	bezeichnung.setCellValueFactory(new PropertyValueFactory<Table, String>("rBezeichnung"));
+	    	preis.setCellValueFactory(new PropertyValueFactory<Table, Double>("rPreis"));
+	    	
+	    	gerichtAuswahl.setItems(data);
+		}
 
 	}
 
--- gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/MainApp.java
+++ gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/MainApp.java
@@ -7,11 +7,17 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 public class MainApp extends Application {
+	
+	
+	public static void main(String[] args) {
+		Application.launch(MainApp.class, (java.lang.String[]) null);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		try {
-			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/fxml/MainWindow.fxml"));
 			Scene scene = new Scene(root,850,570);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -22,8 +28,6 @@ public class MainApp extends Application {
 		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	
 }
 
