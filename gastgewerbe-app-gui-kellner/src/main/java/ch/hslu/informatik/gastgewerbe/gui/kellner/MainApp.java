c9faa24598f73c93c4c11c32fda7c028ea7021e4
iaschwen <thomas.schwendimann@stud.hslu.ch>
2018-05-15 13:03:13 +0200

    commited

--- gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/MainApp.java
+++ gastgewerbe-app-gui-kellner/src/main/java/ch/hslu/informatik/gastgewerbe/gui/kellner/MainApp.java
@@ -1,5 +1,9 @@
 package ch.hslu.informatik.gastgewerbe.gui.kellner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -9,23 +13,31 @@ import javafx.scene.layout.AnchorPane;
 public class MainApp extends Application {
 	
 	
-	public static void main(String[] args) {
-		Application.launch(MainApp.class, (java.lang.String[]) null);
-	}
+	
 	
 	@Override
 	public void start(Stage primaryStage) {
 		
+		Logger logger = LogManager.getLogger(MainApp.class);
+
+		
 		try {
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
 			Scene scene = new Scene(root,850,570);
+			
+			Context.getInstance().setMainStage(primaryStage);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+			logger.error(e);
 		}
 		
+	}
+	public static void main(String[] args) {
+	launch(args);
+		//	Application.launch(MainApp.class, (java.lang.String[]) null);
 	}
 	
 	
